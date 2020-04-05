2bab92b4250b629fffe0330e3befffc822cb0c10
jgrizzled <justin.greene5@gmail.com>
2020-04-04 21:02:12 -0700

    db update

--- src/db-service/timestamps-service.js
+++ src/db-service/timestamps-service.js
@@ -2,26 +2,23 @@
 import db from './db.js';
 
 const readAllTimestamps = async () =>
-  (
-    await db('timestamps')
-      .select('*')
-      .orderBy('timestamp', 'asc')
-  ).map(t => ({ timestamp: Number(t.timestamp) }));
+  (await db('timestamps').select('*').orderBy('timestamp', 'asc')).map((t) => ({
+    timestamp: Number(t.timestamp),
+  }));
 
-const readLatestTimestamp = async () =>
-  Number(
-    (
-      await db('timestamps')
-        .select('*')
-        .orderBy('timestamp', 'desc')
-        .first()
-    ).timestamp
-  );
+const readLatestTimestamp = async () => {
+  const t = await db('timestamps')
+    .select('*')
+    .orderBy('timestamp', 'desc')
+    .first();
+  if (t) return Number(t.timestamp);
+  return null;
+};
 
-const createTimestamps = timestamps =>
+const createTimestamps = (timestamps) =>
   db.batchInsert(
     'timestamps',
-    timestamps.map(timestamp => ({ timestamp }))
+    timestamps.map((timestamp) => ({ timestamp }))
   );
 
 const truncateTimestamps = () => db('timestamps').delete();
@@ -30,5 +27,5 @@ export {
   readAllTimestamps,
   readLatestTimestamp,
   createTimestamps,
-  truncateTimestamps
+  truncateTimestamps,
 };
