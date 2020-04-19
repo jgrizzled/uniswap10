7bb22d583565d1710e934fdd3e1d1ef489170e62
jgrizzled <justin.greene5@gmail.com>
2020-04-19 14:10:28 -0700

    dont update on server start

--- src/server.js
+++ src/server.js
@@ -3,10 +3,28 @@ import env from './config/env.js';
 
 import webService from './web-service/web-service.js';
 import logger from './logger.js';
-import updateDB from './db-service/update-db.js';
+import db from './db-service/db.js';
 
-updateDB().then(() => {
-  webService.listen(env.PORT, () => {
-    logger.info('Web server is listening on port ' + env.PORT);
-  });
+const HTTPserver = webService.listen(env.PORT, () => {
+  logger.info('Web server is listening on port ' + env.PORT);
 });
+
+const closeServer = async () => {
+  console.log('Shutting down');
+  try {
+    await HTTPserver.close(err => {
+      if (err) throw err;
+    });
+    await db.destroy();
+    process.exit(0);
+  } catch (e) {
+    fail(e);
+  }
+};
+
+const fail = e => {
+  console.error(e);
+  process.exit(1);
+};
+
+process.on('SIGINT', closeServer);
